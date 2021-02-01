292ee01b1b8e4dbd0baef0ba3f516e5076e77e64
iwospc <90707576+iwospc@users.noreply.github.com>
2021-02-01 07:34:28 +0000

    Update main.scss

--- chart.php
+++ chart.php
@@ -14,4 +14,4 @@
 		var data = new google.visualization.DataTable();
 		data.addColumn('date', 'Date');
 		data.addColumn('number', 'Buy Price');
-		data.addColumn('number', 'S
+		data.addColumn('number', 'Sal
