5dd86bbd3abd2b9393e99892b63f1d5bdc86a799
iwospc <90707576+iwospc@users.noreply.github.com>
2021-02-01 21:10:17 +0000

    nothing much added

--- chart.php
+++ chart.php
@@ -14,4 +14,4 @@
 		var data = new google.visualization.DataTable();
 		data.addColumn('date', 'Date');
 		data.addColumn('number', 'Buy Price');
-		data.addColumn('number', 'Sal
+		data.addColumn('number', 'Sale 
