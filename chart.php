9399f7c2210f2029fc01054c9dd066b8fb8e81db
iwospc <90707576+iwospc@users.noreply.github.com>
2021-08-28 17:24:57 +0000

    fix duplicate

--- chart.php
+++ chart.php
@@ -21,4 +21,4 @@
 			title: 'Trading Post Prices for <?php echo $itemName ?>'
 		};
 
-		var chart = new google.visualization.LineChart(document.getElementById(
+		var chart = new google.visualization.LineChart(document.getElementById('t
