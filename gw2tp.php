b2bef755f445622fdf3cc654b40b09bfe9dc7abf
iwospc <90707576+iwospc@users.noreply.github.com>
2021-10-17 09:27:41 +0000

    mach64: use unaligned access

--- gw2tp.php
+++ gw2tp.php
@@ -9,3 +9,6 @@ require_once("includes/common.php");
 //	$count = 0;
 	while(! feof($fp)){
 		$item = (fgetcsv($fp));
+		$sql = "INSERT INTO prices (id, time, buy, sell, supply, demand) VALUES ('$item[0]', '$time', '$item[2]', '$item[3]', '$item[4]', '$item[5]')";
+//		print_r($item);
+//		$sql = "INSERT INTO items (id, name) VALUES ('$item[0]', '$item[1]')";
