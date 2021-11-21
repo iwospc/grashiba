67526c8de906f929f81d960edaa45e02a4bc2985
iwospc <90707576+iwospc@users.noreply.github.com>
2021-11-21 08:09:56 +0000

    Added usage information

--- includes/common.php
+++ includes/common.php
@@ -6,3 +6,5 @@ require_once("constants.php");
 // connect to server
 if (($connection = @mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD)) === false)
 	print "could not connect to server";
+
+if (@mysql_select_db(DB_NAME, $connection) === false)
