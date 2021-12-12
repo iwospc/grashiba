8dc35f73a8ee0ad54603d3d6cd142a475a49f864
iwospc <90707576+iwospc@users.noreply.github.com>
2021-12-12 07:53:07 +0000

    second commit

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>The Bazaar</title>
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
