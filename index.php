1b2c15c97e0b59dc6dcd5d11eda9ce236acd45c5
iwospc <90707576+iwospc@users.noreply.github.com>
2021-12-13 18:57:22 +0000

    Code style fix

--- index.php
+++ index.php
@@ -8,3 +8,4 @@
 	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
 	<link href="style.css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="http://code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
