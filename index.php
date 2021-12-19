dd2dcdbcced3af4dbfafe88a2f72887c9bd7aa80
iwospc <90707576+iwospc@users.noreply.github.com>
2021-12-19 15:48:00 +0000

    Creado con Colaboratory

--- index.php
+++ index.php
@@ -10,3 +10,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
 	<script src="https://www.google.com/jsapi"></script>
+	<script src="script.js"></script>
+</head>
+<body>
+	<header>
