22982bb879f08bb59072dd3eaec8f3ae97425e3a
iwospc <90707576+iwospc@users.noreply.github.com>
2021-12-19 07:23:45 +0000

    fixed makefile

--- index.php
+++ index.php
@@ -9,3 +9,4 @@
 	<link href="style.css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
+	<script src="https://www.google.com/jsapi"></script>
