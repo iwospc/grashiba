4d757cd6b78f541f3ef4b705bc9e30d4403e442b
iwospc <90707576+iwospc@users.noreply.github.com>
2022-11-15 12:41:31 +0000

    new rule

--- menu.php
+++ menu.php
@@ -3,4 +3,4 @@
 	<li><a href="index.php?id=19697">Copper Ore</a></li>
 	<li><a href="index.php?id=19748">Silk Scrap</a></li>
 	<li><a href="index.php?id=12138">Stick of Butter</a></li>
-	<li><a href="index.p
+	<li><a href="index.php?id=
