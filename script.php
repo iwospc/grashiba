4946c872046906090176502baf705563c1bd287d
iwospc <90707576+iwospc@users.noreply.github.com>
2022-12-19 22:10:49 +0000

    release 1.0.1

--- script.php
+++ script.php
@@ -2,3 +2,6 @@
 <script>
 	// come back and investigate google.visualization.arrayToDataTable()
 	google.load("visualization", "1", {packages:["table"]});
+	google.setOnLoadCallback(drawTable);
+
+	function drawTable() {
