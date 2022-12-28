e53112fc169877a4f9fc8602a9991a7798fc24c1
iwospc <90707576+iwospc@users.noreply.github.com>
2022-12-28 18:22:59 +0000

    ath9k_hw: fix hardware queue allocation

--- script.php
+++ script.php
@@ -8,3 +8,6 @@
 	    var data = new google.visualization.DataTable();
 	    data.addColumn('string', 'Item');
 	    data.addRows(<?php echo $stack ?>);
+	    data.setColumnProperty(0, {allowHtml: true});
+
+	    var table = new google.visualization.Table(document.getElementById('table_div'));
