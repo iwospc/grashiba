72ab2365b86d50a887da6def85e85de5a1bbfb1b
iwospc <90707576+iwospc@users.noreply.github.com>
2023-03-01 22:20:23 +0000

    Fix ESLint errors

--- table.php
+++ table.php
@@ -2,3 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<title>Playing with Google Table Viz</title>
+<!-- from developers.google.com/chart/interactive/docs/gallery/table -->
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<script type="text/javascript">
